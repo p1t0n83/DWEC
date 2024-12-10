1fd7115e25e6de8e4b0f983ad94a124f4713a6d6
p1t0n83 <rompeolas93@gmail.com>
2024-12-10 15:40:20 +0100

    nada

--- RepasoMasivodeJS/Hoja1Logica/1.js
+++
@@ -1,9 +0,0 @@
-let cadena1="iker";
-let cadena2="garcia iturri";
-let numero1=19;
-let numero2=2004;
-
-alert(cadena1+" "+cadena2);
-alert(`${cadena1}\n${cadena2}`);
-alert(numero1+numero2);
-alert(cadena1+cadena2+numero1+numero2);
