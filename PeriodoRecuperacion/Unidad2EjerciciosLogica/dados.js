c870ccfbb55ce0b71efbd1c247cb5376abc464f4
p1t0n83 <rompeolas93@gmail.com>
2025-04-01 13:18:46 +0200

    ejercicio dados

---
+++ PeriodoRecuperacion/Unidad2EjerciciosLogica/dados.js
@@ -0,0 +1,39 @@
+function jugarDados(numeroLados) {
+  function valor() {
+    return Math.floor(Math.random() * numeroLados) + 1;
+  }
+  return [valor(), valor()];
+}
+
+function tirarDados(numeroLados, tiradas) {
+  let resultado = [];
+  let victoriasJugador = 0;
+  let victoriasMaquina = 0;
+  for (let i = 0; i < tiradas; i++) {
+    resultado.push(jugarDados(numeroLados));
+    if (resultado[i][0] > resultado[i][1]) {
+      resultado[i][2] = "jugador";
+      victoriasJugador += 1;
+    } else if (resultado[i][0] < resultado[i][1]) {
+      resultado[i][2] = "maquina";
+      victoriasMaquina += 1;
+    } else {
+      resultado[i][2] = "empate";
+    }
+    alert(resultado[i]);
+  }
+  alert("Victorias de la maquina: " + victoriasMaquina);
+  alert("Victorias del jugador: " + victoriasJugador);
+}
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let boton=this.getElementById("tirar");
+    boton.addEventListener("click",function(){
+        let numeroLados = parseInt(prompt("Numero de lados"));
+let tiradas = parseInt(prompt("tiradas"));
+
+tirarDados(numeroLados, tiradas);
+    })
+})
