ea3189d5e5826fd2443099d3b3e75b38e603e761
p1t0n83 <rompeolas93@gmail.com>
2025-05-07 16:25:18 +0200

    ahora va

--- PeriodoRecuperacion/Unidad3Callbacks/script.js
+++ PeriodoRecuperacion/Unidad3Callbacks/script.js
@@ -1,21 +1,23 @@
 import { validarNombre, validarPassword } from "./funciones.js";
 
 const $btnGuardar = document.getElementById("guardar");
-const $inpNombre = document.getElementById("nombre").value;
-const $inpPassword = document.getElementById("password").value;
-const $inpEmail = document.getElementById("email").value;
-const $inpFecha = document.getElementById("nacimiento").value;
 
-$btnGuardar.addEventListener("click", function () {
+$btnGuardar.addEventListener("click", function (e) {
+    e.preventDefault();
+    const $inpNombre = document.getElementById("nombre").value;
+    const $inpPassword = document.getElementById("password").value;
+    const $inpEmail = document.getElementById("email").value;
+    const $inpFecha = document.getElementById("nacimiento").value;
 
-    validarNombre($inpNombre, (nombreValidado, rechazada) => {
+
+    validarNombre($inpNombre, (nombreValidado, rechazada) => {    
         if (rechazada) {
-            alert('Error: ' + error);
+            alert('Error: ' + rechazada.message);
             return;
         }
         validarPassword($inpPassword, (passwordValidada, rechazada) => {
             if (rechazada) {
-                alert('Error: ' + error);
+                alert('Error: ' + rechazada.message);
                 return;
             }
             validarEmail($inpEmail, (emailValidado, rechazada) => {
