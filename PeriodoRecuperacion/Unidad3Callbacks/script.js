9acaccc859481ac39c0d6ab378a72b1c42a1da07
p1t0n83 <rompeolas93@gmail.com>
2025-05-13 00:09:06 +0200

    ate a batalha dosmais forces

--- PeriodoRecuperacion/Unidad3Callbacks/script.js
+++ PeriodoRecuperacion/Unidad3Callbacks/script.js
@@ -2,38 +2,53 @@ import * as validaciones from "./funciones.js";
 
 const $btnGuardar = document.getElementById("guardar");
 
+const $inpNombre = document.getElementById("nombre");
+const $inpPassword = document.getElementById("password");
+const $inpEmail = document.getElementById("email");
+const $inpFecha = document.getElementById("nacimiento");
+
+
+  $inpNombre.value = localStorage.getItem("nombre");
+  $inpPassword.value = localStorage.getItem("password");
+  $inpEmail.value = localStorage.getItem("email");
+  $inpFecha.value = localStorage.getItem("nacimiento");
+
+
 $btnGuardar.addEventListener("click", function (e) {
-    e.preventDefault();
-    const $inpNombre = document.getElementById("nombre").value;
-    const $inpPassword = document.getElementById("password").value;
-    const $inpEmail = document.getElementById("email").value;
-    const $inpFecha = document.getElementById("nacimiento").value;
+  e.preventDefault();
+  const $nombre = $inpNombre.value;
+  const $password = $inpPassword.value;
+  const $email = $inpEmail.value;
+  const $fecha = $inpFecha.value;
 
-
-    validaciones.validarNombre($inpNombre, (nombreValidado, rechazada) => {    
+  validaciones.validarNombre($nombre, (nombreValidado, rechazada) => {
+    if (rechazada) {
+      alert("Error: " + rechazada.message);
+      return;
+    }
+    validaciones.validarPassword($password, (passwordValidada, rechazada) => {
+      if (rechazada) {
+        alert("Error: " + rechazada.message);
+        return;
+      }
+      validaciones.validarEmail($email, (emailValidado, rechazada) => {
         if (rechazada) {
-            alert('Error: ' + rechazada.message);
-            return;
+          alert("Error: " + rechazada.message);
+          return;
         }
-        validaciones.validarPassword($inpPassword, (passwordValidada, rechazada) => {
-            if (rechazada) {
-                alert('Error: ' + rechazada.message);
-                return;
-            }
-            validaciones.validarEmail($inpEmail, (emailValidado, rechazada) => {
-                if (rechazada) {
-                    alert('Error: ' + rechazada.message);
-                    return;
-                }
-                validaciones.validarFecha($inpFecha, (fechaValidada, rechazada) => {
-                    if (rechazada) {
-                        alert('Error: ' + rechazada.message);
-                        return;
-                    }
-                    alert("Todos los campos son validos");
-                    
-                })
-            })
+        validaciones.validarFecha($fecha, (fechaValidada, rechazada) => {
+          if (rechazada) {
+            alert("Error: " + rechazada.message);
+            return;
+          }
+          localStorage.setItem("nombre", nombreValidado);
+          localStorage.setItem("password", passwordValidada);
+          localStorage.setItem("email", emailValidado);
+          localStorage.setItem("fecha", fechaValidada);
+
+          alert("Todos los campos son validos");
         });
+      });
     });
-})
+  });
+});
